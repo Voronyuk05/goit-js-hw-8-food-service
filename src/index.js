845bdb230f1e99b6869c116cfcbf73f723199edf
Ivan <vandoslando@gmail.com>
2023-07-24 13:42:38 +0300

    file v1.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import testTpl from './test.handlebars';
+import Handlebars from './templates/handlebars.hbs';
+import menuData from "./menu.json"
 
-document.getElementById('js-creater')
-.addEventListener('click', ()=>{
-    document.body.insertAdjacentHTML(
-        'beforeend', 
-        testTpl({discription: 'This is my template'})
-        )
-})
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitchToggle = document.querySelector('#theme-switch-toggle');
+const menuContainer = document.querySelector('.js-menu');
+
+const applyTheme = (theme) => {
+  document.body.classList.remove(Theme.LIGHT, Theme.DARK);
+  document.body.classList.add(theme);
+  themeSwitchToggle.checked = theme === Theme.DARK;
+  localStorage.setItem('theme', theme);
+};
+
+themeSwitchToggle.addEventListener('click', () => {
+  const selectedTheme = themeSwitchToggle.checked ? Theme.DARK : Theme.LIGHT;
+  applyTheme(selectedTheme);
+});
+
+const init = () => {
+  const savedTheme = localStorage.getItem('theme') || Theme.LIGHT;
+  applyTheme(savedTheme);
+
+  const menuHTML = Handlebars({ menuItems: menuData });
+  menuContainer.innerHTML = menuHTML;
+};
+
+init();
